6cef5193203e5aefe6161a69fffc0a31a2daec3d
= <=>
2020-12-31 22:58:56 +0300

    Remove boilerplate routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv"
 import express from "express";
-import authorizedRoute from "./middleware/authorizedRoute";
 import cookieParser from "cookie-parser"
 import authController from "./controllers/authController";
 import morgan from "morgan"
@@ -11,18 +10,6 @@ app.use(morgan("combined"))
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use("/api/auth", authController)
-
-app.get("/", (_req, res) => {
-  return res.send("Welcome to public area");
-});
-
-app.get("/private", authorizedRoute(), (req, res) => {
-  console.log("Private area accessed by member:")
-  //@ts-ignore
-  console.log(req.user)
-  //@ts-ignore
-  return res.send(`Welcome to private area, ${req.user.username}`);
-});
 
 
 app.listen(process.env.PORT, () =>
